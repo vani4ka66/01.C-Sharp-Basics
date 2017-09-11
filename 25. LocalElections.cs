c1d631d4f47ada03fddf351bb0e1bfc0988eb0e1
Vania Dimitrova <vani4ka66@abv.bg>
2017-09-11 10:57:50 +0300

    Create 25. LocalElections.cs

---
+++ 25. LocalElections.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int candidates = int.Parse(Console.ReadLine());
+            int chosenOne = int.Parse(Console.ReadLine());
+            string symbol = Console.ReadLine();
+            Console.WriteLine(new string('.', 13));
+
+            for (int i = 1; i <= candidates; i++)
+            {
+                Console.WriteLine("...+{0}+...", new string('-', 5));
+
+                if (i == chosenOne)
+                {
+                    if (symbol.ToLower() == "x")
+                    {
+                        Console.WriteLine("...|.\\./.|...");
+                        Console.WriteLine("{1}.|..{0}..|...", symbol.ToUpper(), i.ToString().PadLeft(2, '0'));
+                        Console.WriteLine("...|./.\\.|...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("...|\\.../|...");
+                        Console.WriteLine("{0}.|.\\./.|...", i.ToString().PadLeft(2, '0'));
+                        Console.WriteLine("...|..V..|...");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("...|.....|...");
+                    Console.WriteLine("{0}.|.....|...", i.ToString().PadLeft(2, '0'));
+                    Console.WriteLine("...|.....|...");
+                }
+
+                Console.WriteLine("...+{0}+...", new string('-', 5));
+                Console.WriteLine(new string('.', 13));
+            }
+
+        }
+    }
+}
